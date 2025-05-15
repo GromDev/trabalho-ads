db49560538f3f501fb58fca47f9addb43ce7be67
GromDev <felipe_grom@hotmail.com>
2025-05-14 23:30:41 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector('#botao');
+//botao.style.background = "blue";
+
+botao.addEventListener ("click", e =>{
+
+botao.innerHTML="A persistencia leva ao sucesso!";
+
+});
+
